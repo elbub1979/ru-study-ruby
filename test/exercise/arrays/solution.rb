e05092fda988f67442af37f46aaaa27b77a981b5
elbub1979 <ilya.leuh09@gmail.com>
2023-06-24 18:25:47 +0300

    refactoring array tests (test_replace, test_bin_search)

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,34 +2,46 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
+        max_value = max_value(array)
+
+        array.each_with_object([]) do |element, final_array|
+          final_array << (element.positive? ? max_value : element)
+        end
+      end
+
+      def search(array, query, min = 0, max = array.size - 1)
+        return -1 if array.empty? || wrong_value?(array, query, min, max) || out_of_array?(array, query, min, max)
+
+        mid = (min + max) / 2
+
+        return mid if array[mid] == query
+
+        array[mid] < query ? min = mid + 1 : max = mid - 1
+
+        search(array, query, min, max)
+      end
+
+      private
+
+      def max_value(array)
         max_value = array[0]
-        final_array = []
 
         array.each do |element|
           max_value = element if element > max_value
         end
-
-        array.each do |element|
-          final_array << (element.positive? ? max_value : element)
-        end
-        final_array
+        max_value
       end
 
-      def search(_array, _query, min = 0, max = _array.size - 1)
-        return -1 if _array.empty?
-        return -1 if min == max && _array[min] != _query
+      def wrong_value?(*args)
+        array, query, min, max = args
 
-        mid = (min + max) / 2
+        array[min] == array[max] && array[min] != query
+      end
 
-        case _array[mid] <=> _query
-        when 0
-          return mid
-        when -1
-          min = mid + 1
-        when 1
-          max = mid - 1
-        end
-        search(_array, _query, min, max)
+      def out_of_array?(*args)
+        array, query, min, max = args
+
+        array[min] > query || array[max] < query
       end
     end
   end
