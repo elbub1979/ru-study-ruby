077c3232b73a2a61aecb5f5e12d97fd25e21e957
elbub1979 <ilya.leuh09@gmail.com>
2023-06-25 23:50:40 +0300

    add custom methods (my_each, my_reduce, my_map, my_compact)

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,44 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each(&block)
+        return to_enum unless block_given?
+
+        for element in self
+          block.call(element)
+        end
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map(&block)
+        return to_enum unless block_given?
+
+        my_reduce(self.class.new) { |accumulator, element| accumulator << block.call(element) }
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        my_reduce(self.class.new) do |accumulator, element|
+          element.nil? ? accumulator : accumulator << element
+        end
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(accumulator = nil, &block)
+        return accumulator if size.zero?
+
+        accumulator = if accumulator.nil?
+                        first
+                      else
+                        block.call(accumulator, first)
+                      end
+
+        self[1..].each do |element|
+          accumulator = block.call(accumulator, element)
+        end
+
+        accumulator
+      end
     end
   end
 end
